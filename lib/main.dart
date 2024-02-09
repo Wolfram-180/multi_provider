0ab16c2890620c0e2a969726a911a1c4cbffd030
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-09 22:05:06 +0300

    MultiProvider works

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -15,6 +16,50 @@ void main() {
 
 String now() => DateTime.now().toIso8601String();
 
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = now();
+}
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        color: Colors.blue,
+        height: 100,
+        child: Text(seconds.value),
+      ),
+    );
+  }
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = now();
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        color: Colors.green,
+        height: 100,
+        child: Text(minutes.value),
+      ),
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -23,6 +68,34 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home page'),
+      ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            initialData: Seconds(),
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+          ),
+          StreamProvider.value(
+            initialData: Minutes(),
+            value: Stream<Minutes>.periodic(
+              const Duration(seconds: 5),
+              (_) => Minutes(),
+            ),
+          ),
+        ],
+        child: const Column(
+          children: [
+            Row(
+              children: [
+                SecondsWidget(),
+                MinutesWidget(),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
