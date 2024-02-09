5d992c06341f3365a124dec1d46d5d795c855728
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-09 21:13:31 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/multi_provider_course_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multi_provider_course_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Test app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+String now() => DateTime.now().toIso8601String();
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+    );
+  }
+}
